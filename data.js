cc3d066e9c6344096d3b268cf9cd58a1b9184bc7
gh0st1ng74 <140424259+gh0st1ng74@users.noreply.github.com>
2024-01-31 16:07:04 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+// Bien mettre musics/et le nom du fichier, et images/couvertures/et le nom de la couverture
+
+let songs = [
+    {
+        name: 'song 1',
+        path: 'musics/music1.mp3',
+        artist: 'artist 1',
+        cover: 'images/couvertures/cover.jpg'
+    },
+    {
+        name: 'song 2',
+        path: 'musics/music2.mp3',
+        artist: 'artist 2',
+        cover: 'images/couvertures/cover2.jpeg'
+    },
+    {
+        name: 'song 3',
+        path: 'musics/music3.mp3',
+        artist: 'artist 3',
+        cover: 'images/couvertures/cover3.jpeg'
+    },
+    {
+        name: 'song 4',
+        path: 'musics/music4.mp3',
+        artist: 'artist 4',
+        cover: 'images/couvertures/cover4.jpeg'
+    }
+]
